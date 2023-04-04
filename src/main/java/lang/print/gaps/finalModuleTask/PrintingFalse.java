556ce6106948409318103a8eff34d35a6b8ea9e1
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-04 15:05:11 +0530

    #1

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -1,9 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.Arrays;
+
 public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
-        //write code here
+        System.out.println(first == second);
     }
 }
